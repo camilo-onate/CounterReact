25882fd6fcd0f38f12d4ef6caf65d7b15b2f424f
Camilo Oñate Moraga <camiloonatemoraga@MacBook-Pro-de-Camilo.local>
2019-05-25 11:13:38 -0400

    countercorregido

--- hello-rigo/src/js/component/counter.jsx
+++ hello-rigo/src/js/component/counter.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 const Counter = () => {
     return (
         
+        let seconds = 0;
+
+        function init(){
+            setInterval(newseconds,1000);
+        }
+
+        function newseconds(){
+            seconds = seconds+1;
+        }
     
     )
 }
