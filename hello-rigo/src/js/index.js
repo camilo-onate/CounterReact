93ab18daad39cc6ddbe27e5283b8a5512465f01f
VANDRIANAMW <VANDRIANAMW@gmail.com>
2019-05-26 23:37:25 +0000

    commit

--- hello-rigo/src/js/index.js
+++ hello-rigo/src/js/index.js
@@ -1,17 +1,41 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropType from "props-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
+
+import './icons.js';
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
 //import your own components
 function SimpleCounter(props){
-    return (<p>Hello world</p>);
+    return (<div className="bigCounter">
+            <div className="calendar">
+                <i class="far fa-clock"></i>
+            </div>
+            <div className="secondsSeven">{props.digitSeven}</div>
+            <div className="secondsSix">{props.digitSix}</div>
+            <div className="secondsFive">{props.digitFive}</div>
+            <div className="secondsFour">{props.digitFour}</div>
+            <div className="secondsThree">{props.digitThree}</div>
+            <div className="secondsTwo">{props.digitTwo}</div>
+            <div className="secondsOne">{props.digitOne}</div>
+    </div>);
 }
 
+let count= 0;
+setInterval(function(){
+const seven=Math.floor(counter/10000000);
+const six=Math.floor(counter/1000000);
+const five=Math.floor(counter/100000);
+const four=Math.floor(counter/10000);
+const three=Math.floor(counter/1000);
+const two=Math.floor(counter/100);
+const one=Math.floor(counter/10);
 //render your react application
-ReactDOM.render(<SimpleCounter/>, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter digitOne={}/>, document.querySelector("#app"));
+},1000);
