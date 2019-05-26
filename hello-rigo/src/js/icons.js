d02f1addf920431055d5fb9e6b77d17734478444
VANDRIANAMW <VANDRIANAMW@gmail.com>
2019-05-26 21:21:26 +0000

    importando iconos

---
+++ hello-rigo/src/js/icons.js
@@ -0,0 +1,8 @@
+import fontsawesome from '@fortawesome/fontawesome';
+
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+
+fontsawesome.config = {
+    autoReplaceSvg: 'nest'
+}
+fontsawesome.library.add(faClock);
